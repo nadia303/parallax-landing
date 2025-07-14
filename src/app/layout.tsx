54885ee18dcc9e8d131563a0d89cdc0a90f80acb
Nadiia Mishchuk <nadiia_mishchuk@pecodesoftware>
2025-07-14 12:13:09 +0300

    Added global stylings and necessary fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Inter, Metrophobic } from "next/font/google";
+import "./globals.scss";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--font-inter",
+  display: "swap",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const metrophobic = Metrophobic({
   subsets: ["latin"],
+  weight: "400",
+  variable: "--font-metrophobic",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -24,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${inter.variable} ${metrophobic.variable}`}>
         {children}
       </body>
     </html>
