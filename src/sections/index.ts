10f58c1229cacd1107bdefff77b80fa6b9c329f5
Nadiia Mishchuk <nadiia_mishchuk@pecodesoftware>
2025-07-14 12:34:34 +0300

    Added all sections to main page

---
+++ src/sections/index.ts
@@ -0,0 +1,5 @@
+export { default as BuildTheBestAI } from "./BuildTheBestAI/BuildTheBestAI";
+export { default as Hero } from "./Hero/Hero";
+export { default as LeaderboardTable } from "./LeaderboardTable/LeaderboardTable";
+export { default as Projects } from "./Projects/Projects";
+export { default as TextComponent } from "./TextComponent/TextComponent";
