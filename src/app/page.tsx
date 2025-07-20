91aec4786181a644d64fbc644bfeceb56dfcceb8
Nadiia Mishchuk <nadiia_mishchuk@pecodesoftware>
2025-07-20 12:34:26 +0300

    Added Lenis and some main page stucture changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,44 @@
-import Image from "next/image";
+import classNames from "classnames";
+import ReactLenis from "lenis/react";
 import styles from "./page.module.scss";
 import { Footer, Header } from "@/components/UI";
-import { BuildTheBestAI, Hero, LeaderboardTable, Projects, TextComponent } from "@/sections";
+import {
+  BuildTheBestAI,
+  Hero,
+  LeaderboardTable,
+  Projects,
+  TextComponent,
+} from "@/sections";
 
 export default function Home() {
   return (
     <div>
-      <Header />
       <main>
-        <div className={styles.containerWithPlanet}>
-          <Image
-            src="/pictures/planet.webp"
-            alt="Planet Image"
-            width={776}
-            height={776}
-            className={styles.stickyImage}
-          />
-          <Hero />
-          <BuildTheBestAI />
-          <Projects />
-          <LeaderboardTable />
-        </div>
-        <TextComponent />
+        <ReactLenis root options={{ duration: 1.5 }}>
+          <div className={classNames(styles.containerWithPlanet)}>
+            <Header />
+            <div
+              className="container"
+              style={{
+                position: "sticky",
+                top: 0,
+                height: "120vh",
+                width: "100%",
+                backgroundImage: "url(/pictures/background.webp)",
+                backgroundPosition: "top right",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+              }}
+            ></div>
+            <Hero />
+            <Projects />
+            <BuildTheBestAI />
+          </div>
+          <div>
+            <LeaderboardTable />
+            <TextComponent />
+          </div>
+        </ReactLenis>
       </main>
       <Footer />
     </div>
