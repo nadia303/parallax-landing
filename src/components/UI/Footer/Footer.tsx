196c21f996d617415fe93afa0d06e2aac1bcd839
Nadiia Mishchuk <nadiia_mishchuk@pecodesoftware>
2025-07-20 12:30:58 +0300

    Added mobile view of Header and Footer

--- src/components/UI/Footer/Footer.tsx
+++ src/components/UI/Footer/Footer.tsx
@@ -1,28 +1,28 @@
 import Link from "next/link";
-import styles from "./Footer.module.scss";
 import classNames from "classnames";
+import styles from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <footer className={classNames(styles.footerContainer, "full-width")}>
       <div className={classNames(styles.footerContentContainer, "container")}>
-          <nav className={styles.footerCenter}>
-            <Link href="/">Home</Link>
-            <Link href="/leaderboard">Leaderboard</Link>
-            <Link href="/about">About</Link>
-          </nav>
+        <nav className={styles.footerCenter}>
+          <Link href="/">Home</Link>
+          <Link href="/leaderboard">Leaderboard</Link>
+          <Link href="/about">About</Link>
+        </nav>
         <div className={styles.footerDivider}></div>
         <div className={styles.footerBottom}>
-        <div className={styles.footerLeft}>
-          <span className={styles.dot}></span>
-          <span className={styles.dot}></span>
-          <span className={styles.dot}></span>
-        </div>
-        <div className={styles.footerRight}>
-          <Link href="#">Terms of Use</Link>
-          <Link href="#">Privacy Policy</Link>
-          <Link href="#">Cookie Policy</Link>
-        </div>
+          <div className={styles.footerLeft}>
+            <span className={styles.dot}></span>
+            <span className={styles.dot}></span>
+            <span className={styles.dot}></span>
+          </div>
+          <div className={styles.footerRight}>
+            <Link href="#">Terms of Use</Link>
+            <Link href="#">Privacy Policy</Link>
+            <Link href="#">Cookie Policy</Link>
+          </div>
         </div>
       </div>
     </footer>
--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,13 +1,37 @@
+"use client";
+
+import classNames from "classnames";
+import { useRef, useState } from "react";
+import { motion, useMotionValueEvent, useScroll } from "motion/react";
 import styles from "./Header.module.scss";
 import Button from "../Button/Button";
-import classNames from "classnames";
 
 const Header = () => {
+  const [isFixed, setIsFixed] = useState(true);
+  const ref = useRef(null);
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    setIsFixed(latest < 200);
+  });
+
   return (
-    <header className={classNames(styles.headerContainer, "container")}>
-      <Button>LLM Leaderboard</Button>
-      <Button variant="outline">Buy Spice AI</Button>
-    </header>
+    <motion.div
+      ref={ref}
+      className={classNames(
+        styles.headerContainer,
+        {
+          [styles.fixed]: isFixed,
+          [styles.absolute]: !isFixed,
+        },
+        "container"
+      )}
+    >
+      <Button size="medium">LLM Leaderboard</Button>
+      <Button variant="outline" size="medium">
+        Buy Spice AI
+      </Button>
+    </motion.div>
   );
 };
 
